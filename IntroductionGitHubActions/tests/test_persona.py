b3222cdc36f6c1affcd7998a44697e040bc930f6
Ivan Penaloza <ivan1016017@gmail.com>
2021-03-14 11:34:25 -0600

    new line added

--- IntroductionGitHubActions/tests/test_persona.py
+++ IntroductionGitHubActions/tests/test_persona.py
@@ -44,4 +44,4 @@ class PersonaTestCase(unittest.TestCase):
 
    def test_instancia_clase(self):
       self.assertIsInstance(self.persona1, Persona)
-      self.assertNotIsInstance(self.grupo, Persona)
+      self.assertNotIsInstance(self.grupo, Persona) # new line
